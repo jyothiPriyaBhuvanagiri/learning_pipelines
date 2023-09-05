6f66aa5a2c673f470b6c01a957f6e4c15e839829
Jyothi <bjyothipriya.999@gmail.com>
2023-09-05 09:42:10 +0200

    initial commit

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello')
 print(" rabbit says  'Hello World!'")
 connection.close()
